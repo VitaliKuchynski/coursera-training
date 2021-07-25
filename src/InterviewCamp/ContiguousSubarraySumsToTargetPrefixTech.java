9e775cbbd583e206353ef45e29799ac1cae08757
Vitali Kuchynski <vitali_kuchynski@epam.com>
2021-07-25 13:04:33 -0400

    prefix

--- src/InterviewCamp/ContiguousSubarraySumsToTargetPrefixTech.java
+++ src/InterviewCamp/ContiguousSubarraySumsToTargetPrefixTech.java
@@ -14,81 +14,9 @@ public class ContiguousSubarraySumsToTargetPrefixTech {
         int[] input = {2, 4, -2, 1, -3, 5, -3, 1};
         int target = 4;
 
-        System.out.println(Arrays.toString(subTarg(input, target)));
+        System.out.println(Arrays.toString(targetSumSubarray(input, target)));
 
     }
-
-
-
-public static int[] subTarg(int[] ints, int target) {
-
-        if (ints.length == 0 || ints == null) {
-            return null;
-        }
-
-        int[] result = new int[2];
-
-        int currentSum = 0;
-        Map<Integer, Integer> count = new HashMap<>();
-
-         for (int i = 0; i < ints.length - 1; i++) {
-             currentSum += ints[i];
-
-             if (currentSum == target) {
-                 result[0] = 0;
-                 result[1] = i;
-                 return result;
-             }
-
-         if (count.containsKey(currentSum - target)){
-
-             result[0] = count.get(currentSum - target) + 1;
-             result[1] = i;
-             return result;
-         }
-            count.put(currentSum, i);
-         }
-        return null;
-}
-
-    public static int[] toTargetInArray(int[] ints, int target) {
-
-        if (ints == null || ints.length == 0) {
-            return null;
-        }
-
-        int sum = 0;
-        Map<Integer, Integer> count = new HashMap<>();
-        int[] result  = new int[2];
-
-        for (int i = 0; i < ints.length; i++) {
-            sum += ints[i];
-
-            if (sum == target){
-                result[0] = 0;
-                result[1] = i;
-                return result;
-            }
-
-            if (count.containsKey(sum - target)) {
-                result[0] = count.get(sum - target) + 1;
-                result[1] = i;
-                return result;
-            } else
-                count.put(sum, i);
-        }
-        return null;
-    }
-
-
-
-
-
-
-
-
-
-
 
     public static int[] targetSumSubarray(int[] ints, int target) {
 
