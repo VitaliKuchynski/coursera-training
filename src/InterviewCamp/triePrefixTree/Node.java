319b9958d9af4453a533634b9d7ac8bd739f6fab
vitalikuchynski <vitali.kuchynski@gmail.com>
2023-06-28 14:42:06 -0400

    trie

---
+++ src/InterviewCamp/triePrefixTree/Node.java
@@ -0,0 +1,41 @@
+package InterviewCamp.triePrefixTree;
+
+import java.util.HashMap;
+
+public class Node {
+
+    HashMap<Character , Node> map;
+    boolean isWord;
+
+    public Node() {
+        this.map = new HashMap<>();
+        this.isWord = false;
+    }
+
+
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public void setWord(boolean word) {
+        isWord = word;
+    }
+
+    public boolean contains(char ch) {
+        return map.containsKey(ch);
+    }
+
+    public void addNode(char ch) {
+        if (!map.containsKey(ch)) {
+            map.put(ch, new Node());
+        }
+    }
+
+    public Node getNode(char ch) {
+        return map.get(ch);
+    }
+
+    public HashMap<Character, Node> getMap() {
+        return map;
+    }
+}
